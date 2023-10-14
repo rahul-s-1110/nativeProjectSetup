9d10af595e5919ada43b64774c75dba7afa5e7f4
rahul-s-1110 <saini1792@gmail.com>
2023-10-14 22:18:45 +0530

    overlapping

--- App.js
+++ App.js
@@ -1,13 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import RootNavigation from './src/navigation/rootNavigation';
+import { Provider } from 'react-redux';
+import { store } from './src/redux/store';
 
 export default function App() {
   return (
     <View style={styles.container}>
       {/* <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" /> */}
+      <Provider store={store}>
       <RootNavigation />
+      </Provider>
     </View>
   );
 }
