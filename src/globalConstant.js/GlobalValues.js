60d23d06d35638361120137a2f295f6d2562d730
rahul-s-1110 <saini1792@gmail.com>
2023-10-15 07:00:46 +0530

    setup of image,fonts,context and global value

---
+++ src/globalConstant.js/GlobalValues.js
@@ -0,0 +1,38 @@
+import {Dimensions} from 'react-native';
+
+export const ACCESS_TOKEN = 'ACCESS_TOKEN';
+export const SELECTED_LANGAUAGE = 'SELECTED_LANGAUAGE';
+export const EMAIL = 'EMAIL';
+export const SIGN_OUT = 'SIGN_OUT';
+export const USER_ID = 'USER_ID';
+export const USER_PHONE_NUMBER = 'USER_PHONE_NUMBER';
+export const USED_LANGUAGE = 'USED_LANGUAGE';
+
+export const TYPE = 'TYPE';
+export const USER_TYPE = 'USER_TYPE';
+
+export const PROFILE_SUCCESS = 'PROFILE_SUCCESS';
+export const TOKEN = 'login_token';
+export const PROFILE_STATUS = 'PROFILE_STATUS';
+export const VERIFY_ACCOUNT = 'VERIFY_ACCOUNT';
+export const RESTORE_TOKEN = 'RESTORE_TOKEN';
+export const USER_ROLE = 'USER_ROLE';
+export const PROFILE = 'PROFILE';
+export const SIGN_IN = 'SIGN_IN';
+export const COMPLETE_PROFILE = 'COMPLETE_PROFILE';
+export const PROFESSIONAL_AVAIABILTY = 'professional_availabilty';
+export const USER_DATA = 'user_data';
+export const TEMP_USER_DATA = 'temp_user_data';
+export const FIRST_TIME_USER = 'First_time_user';
+export const FIRST_TIME_LANGUAGE = 'First_time_language';
+export const USER_INTRO = 'User_Intro';
+export const GUEST_USER = 'GUEST_USER';
+export const FCM_TOKEN = 'fcm_token';
+export const SEARCH_DATA = 'SEARCH_DATA';
+export const firebaseToken = 'FirbaseToken';
+export const PLAN_NAME = 'PLAN_NAME';
+export const USER_LOCATION = 'USER_LOCATION';
+export const DEFAULT_ADDRESS = 'DEFAULT_ADDRESS';
+export const PHONE_NUMBER = 'PHONE_NUMBER';
+export const deviceWidth = Dimensions.get('screen').width;
+export const deviceHeight = Dimensions.get('screen').height;
---
+++ src/globalConstant.js/imagePath.js
@@ -0,0 +1,3 @@
+export const Images = {
+  add: require('../assets/image/add.png'),
+};
